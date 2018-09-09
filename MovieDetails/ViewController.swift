728f89aba57f0ed0753595a1879a6f21f4ae82e5
Mansi Barodia <mansi.ece06@gmail.com>
2018-09-09 12:04:40 -0500

    Initial Commit

---
+++ MovieDetails/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieDetails
+//
+//  Created by Mansi Barodia on 9/9/18.
+//  Copyright © 2018 Mansi Barodia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
